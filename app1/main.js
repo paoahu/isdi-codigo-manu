8c41d737e9e2a8f836c8172c0c5636559f3336c2
Anna Cuenca <acuencabo@gmail.com>
2023-09-26 17:37:46 +0200

    add app behavioir for register

--- app1/main.js
+++ app1/main.js
@@ -1,1 +1,71 @@
-document.getElementById('register').style.display = 'none'
+// storage
+
+var users = []
+
+// register
+
+var registerView = document.getElementById('register')
+
+registerView.style.display = 'none'
+
+var registerLoginLink = registerView.querySelector('a')
+
+registerLoginLink.onclick = function (event) {
+    event.preventDefault()
+
+    registerView.style.display = 'none'
+    loginView.style.display = 'block'
+}
+
+var registerForm = registerView.querySelector('form')
+
+registerForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var nameInput = registerForm.querySelector('#name')
+    var emailInput = registerForm.querySelector('#email')
+    var passwordInput = registerForm.querySelector('#password')
+
+    var name = nameInput.value
+    var email = emailInput.value
+    var password = passwordInput.value
+
+    var user = {}
+
+    user.name = name
+    user.email = email
+    user.password = password
+
+    // TODO check user is new, otherwise show error
+
+    users.push(user)
+
+    nameInput.value = ''
+    emailInput.value = ''
+    passwordInput.value = ''
+
+    registerView.style.display = 'none'
+    loginView.style.display = 'block'
+}
+
+// login
+
+var loginView = document.getElementById('login')
+var loginRegisterLink = loginView.querySelector('a')
+
+loginRegisterLink.onclick = function (event) {
+    event.preventDefault()
+
+    loginView.style.display = 'none'
+    registerView.style.display = 'block'
+
+    // TODO implement login functionality
+}
+
+// home
+
+var homeView = document.getElementById('home')
+
+homeView.style.display = 'none'
+
+// TODO show user name logged in when entering in Home (Hello, >name<!)
